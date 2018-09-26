79b0584f74275166d7dae667486de3c1a54cbe2c
Robert Lockerd <robertl@gmail.com>
2018-09-26 21:30:16 +1200

    Additional spec unit tests

--- spec/laminar/context_spec.rb
+++ spec/laminar/context_spec.rb
@@ -1,5 +1,78 @@
 module Laminar
   RSpec.describe Context do
-    pending "add some examples to (or delete) #{__FILE__}"
+    describe '.build' do
+      let(:context) { described_class.build(x: 1, y: 2, z: 3) }
+
+      it 'returns a new context' do
+        context = Laminar::Context.build(x: 120)
+        expect(context).to be_a(Laminar::Context)
+        expect(context).to include(x: 120)
+      end
+    end
+
+    describe '#halt' do
+      let(:context) { described_class.build(x: 1, y: 2, z: 3) }
+
+      it 'marks the context halted' do
+        expect {
+          context.halt
+        }.to change { context.halted? }.from(false).to(true)
+      end
+
+      it 'merges hashed arguments into context' do
+        context.halt(status: :done)
+        expect(context).to include(status: :done)
+      end
+
+      it 'preserves success' do
+        context.halt
+        expect(context.success?).to be true
+      end
+
+      it 'does not change failure status' do
+        expect {
+          context.halt
+        }.to_not change { context.failed? }
+      end
+    end
+
+    describe '#fail!' do
+      let(:context) { described_class.build(x: 1, y: 2, z: 3) }
+
+      it 'raises an error' do
+        expect {
+          context.fail!
+        }.to raise_error(Laminar::ParticleFailed)
+      end
+
+      it 'marks the context failed' do
+        expect {
+          context.fail! rescue Laminar::ParticleFailed
+        }.to change { context.failed? }.from(false).to(true)
+      end
+
+      it 'sets success false' do
+        expect {
+          context.fail! rescue Laminar::ParticleFailed
+        }.to change { context.success? }.from(true).to(false)
+      end
+
+      it 'marks the context halted' do
+        expect {
+          context.fail! rescue Laminar::ParticleFailed
+        }.to change { context.halted? }.from(false).to(true)
+      end
+
+      it 'merges arguments into context' do
+        context.fail!(status: :done) rescue Laminar::ParticleFailed
+        expect(context).to include(status: :done)
+      end
+
+      it 'does not affect original context' do
+        expect {
+          context.fail!(status: :done) rescue Laminar::ParticleFailed
+        }.to_not change { context[:x] }
+      end
+     end
   end
 end
--- spec/laminar/flow_spec.rb
+++ spec/laminar/flow_spec.rb
@@ -1,5 +1,34 @@
 module Laminar
   RSpec.describe Flow do
-    pending "add some examples to (or delete) #{__FILE__}"
+    let(:flow) { Class.new.send(:include, Laminar::Flow) }
+
+    describe '#flow' do
+      context 'when passed a block containing a flow' do
+        let(:flow_block) { Proc.new { flow { step :step1 } } }
+        let(:spec) { instance_double('Laminar::Flow::Specification') }
+
+        it 'creates a flow specification' do
+          expect(Laminar::Flow::Specification).to receive(:new).once { spec }
+          flow.flow(&flow_block)
+          expect(flow.flowspec).to eq(spec)
+        end
+      end
+    end
+
+    describe '#call!' do
+      it 'invokes the first specified step' do
+      end
+
+      it 'raises an error if a step fails' do
+      end
+    end
+
+    describe '#call' do
+      it 'invokes the first specified step' do
+      end
+
+      it 'silently stops execution if a step fails' do
+      end
+    end
   end
 end
